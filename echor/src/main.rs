679a2f246c5d5788e7a394fe8b681342059146d8
Raymond Ling <rayling.email@gmail.com>
2024-05-15 13:48:10 +0800

    [2024-05-15] use derive attribute

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,26 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version)]
+/// Rust version of 'echo'
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("Raymond Ling <rayling@hotmail.com>")
-        .about("Rust echo")
-        .arg(
-            Arg::new("text")
-            .value_name("TEXT")
-            .help("Input text")
-            .required(true)
-            .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-            .short('n')
-            .action(ArgAction::SetTrue)
-            .help("Do not print the new line"),
-        )
-        .get_matches();
-    let text: Vec<String> =
-        matches.get_many("text").unwrap().cloned().collect();
-    let omit_newline = matches.get_flag("omit_newline");
-    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
+    let args = Args::parse();
+    //dbg!(args);
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else {"\n"}
+    );
 }
